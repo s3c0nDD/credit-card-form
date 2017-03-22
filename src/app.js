1a0ce357de23d8c602d85b5d6e46f1f63ce63b9b
s3c0nDD <s3c0nDD@users.noreply.github.com>
2017-03-22 16:50:09 +0100

    email checkbox handler

--- src/app.js
+++ src/app.js
@@ -1,2 +1,24 @@
-let hi = `hey ho`;
-console.log(hi, `let's go!`);
+$(document).ready(() => {
+
+    const config = {
+        $form: $('#form'),
+        $chkbox: $('#storeTheCard'),
+        $colEmail: $('.col-email')
+    };
+
+    console.group('objects in config');
+    for (let prop in config) console.log(prop)
+    console.groupEnd();
+
+    /* hide email field */
+    config.$colEmail.hide();
+
+    /* handle email checkbox */
+    config.$chkbox.click(()=>{
+        let isChecked = config.$chkbox.is(':checked');
+        (isChecked) ?
+            config.$colEmail.show() :
+            config.$colEmail.hide();
+    });
+
+});
