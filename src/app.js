c007566f7d3cc104f81e9bba3e9559a4f93d71b9
s3c0nDD <s3c0nDD@users.noreply.github.com>
2017-03-22 17:19:28 +0100

    validation mock: all wrong :D

--- src/app.js
+++ src/app.js
@@ -2,23 +2,45 @@ $(document).ready(() => {
 
     const config = {
         $form: $('#form'),
-        $chkbox: $('#storeTheCard'),
-        $colEmail: $('.col-email')
+        $checkbox: $('#storeTheCard'),
+        $colEmail: $('.col-email'),
+        $button: $('#submitBtn')
+    };
+
+    const inputs = {
+        $cardNr: $('#cardNumber'),
+        $expDate: $('#expirationDate'),
+        $cvvNumber: $('#cvvNumber'),
+        $email: $('#userEmail'),
     };
 
     console.group('objects in config');
-    for (let prop in config) console.log(prop)
+    for (let el in config) console.log(el);
+    console.groupEnd();
+    console.group('objects in inputs');
+    for (let el in inputs) console.log(el);
     console.groupEnd();
 
-    /* hide email field */
-    config.$colEmail.hide();
-
     /* handle email checkbox */
-    config.$chkbox.click(()=>{
-        let isChecked = config.$chkbox.is(':checked');
+    config.$checkbox.click(()=>{
+        let isChecked = config.$checkbox.is(':checked');
         (isChecked) ?
             config.$colEmail.show() :
             config.$colEmail.hide();
     });
+
+    /* handle valildation */
+    config.$button.on('click', () => {
+        console.log('submit click');
+        validation();
+    });
+
+    let validation = () => {
+        inputs.$cardNr.parent().addClass('has-error');
+        inputs.$expDate.parent().addClass('has-error');
+        inputs.$cvvNumber.parent().addClass('has-error');
+        inputs.$email.parent().addClass('has-error');
+    }
+
 
 });
